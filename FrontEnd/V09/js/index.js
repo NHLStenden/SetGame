bc534ab2a7ccfe13256a9acc3bc2b9f77997494a
martinmolema <martin@molema.org>
2021-08-27 22:15:27 +0200

    Herstructurering van de code om beter met een asynchrone backend om te gaan - andere methode om HTML kaart aan interne administratie te koppelen

--- FrontEnd/V09/js/index.js
+++ FrontEnd/V09/js/index.js
@@ -6,7 +6,6 @@ const colors = ["red", "green", "purple"];
 const fills = ["solid", "hollow", "striped"];
 const shapes = ["diamond", "pill", "wave"]
 const nrOfShapes = [1, 2, 3];
-let fullSetOfCards ;
 let cardsOnTheBoard;
 let hintsUsed = 0 ;
 let nrOfAttempts = 0 ;
@@ -19,10 +18,14 @@ window.onload = () => {
     setup();
 }
 
+/**
+ * Show how many cards are left in the set.
+ */
 function showCardsLeftInFullSet(){
     const div = document.getElementById("cardsleft");
     if (div) {
-        div.innerHTML = `Cards left: <span>${fullSetOfCards.length}</span>`;
+        // TODO: show real amount (not received from back-end yet)
+        div.innerHTML = `Cards left: <span>0</span>`;
     }
 }
 
@@ -48,34 +51,47 @@ function GetRandomItems(nrOfItemsToTake) {
     return Promise.all(requests);
 } // GetRandomItems
 
+/**
+ * Setup everything.
+ */
 function setup() {
-    setupBoard();
-    manageBoard();
-    setupHinting();
-    setupButtons();
+    // Setupboard returns a promise because data get from HTTP (async)
+    setupBoard().then(value => {
+        manageBoard();
+        setupHinting();
+        setupButtons();
 
-    showNrOfTurns();
-    showCardsLeftInFullSet();
-    showHintsUsed();
+        showNrOfTurns();
+        showCardsLeftInFullSet();
+        showHintsUsed();
+    });
 }// setup
 
 function setupBoard() {
     cardsOnTheBoard = [];
-    fullSetOfCards  = [];
-
-    httpService.getNewDeck().then(
-        (data) => {
-            idDeck = data.deckId;
-            fullSetOfCards = data.cards;
-            ProcessCardsFromBackend(fullSetOfCards);
-            addNewRandomCards(12);
-        },
-        (error) => {
-            console.error(error.statusText);
-        }
-    );
+    return new Promise((resolve, reject) => {
+        httpService.getNewDeck().then(
+            (data) => {
+                idDeck = data.deckId;
+                addNewRandomCards(12).then(value => {
+                    ProcessCardsFromBackend(cardsOnTheBoard);
+                    resolve();
+                });
+            },
+            (error) => {
+                console.error(error.statusText);
+            }
+        );
+    },
+        (error) => { reject(error)
+    });
 }// setupBoard
 
+/**
+ * Processes the cards retrieved from the backend so that an HTML property is added to each
+ * card that can be added later.
+ * @param cards
+ */
 function ProcessCardsFromBackend(cards){
     let cardNr = 1;
     for (const card of cards) {
@@ -90,12 +106,13 @@ function ProcessCardsFromBackend(cards){
                 </svg>`;
             shapes += shapeTemplate;
         }// for items on card
-        const cardTemplate = `<div id="${cardNr}" class="card ${card.shape}">${shapes}</div>`;
+        const uniqueID = createHTMLidFromCardProperties(card);
+        const cardTemplate = `<div id="${uniqueID}" class="card ${card.shape}">${shapes}</div>`;
         card.html = cardTemplate;
-        card.id = cardNr;
+        card.id = createHTMLidFromCardProperties(card);
         cardNr++;
     }
-}
+}//ProcessCardsFromBackend
 
 function addCards(cards) {
     // get the container from the HTML
@@ -128,7 +145,6 @@ function handleClickEvent(evt) {
         }
         else{
             if (cardsSelected.length < 3 ) {
-                console.log(`Clicked on a card! ${clickedElement.id}`);
                 clickedElement.classList.toggle('selected');
             }
         }
@@ -147,7 +163,7 @@ function handleClickEvent(evt) {
                 const cardsToAdd = 12 - cardsOnTheBoard.length;
 
                 if (cardsToAdd > 0 ) {
-                    addNewRandomCards(cardsToAdd);
+                    addNewRandomCards(cardsToAdd).then();
                 }
 
                 clearTableWithSelectionInfo();
@@ -195,9 +211,10 @@ function RemoveSelectedCards(){
 
     // remove cards in the set
     for (const card of cardsNewSelected) {
-        const id = parseInt(card.id, 10);
         card.parentElement.removeChild(card);
-        const idx = cardsOnTheBoard.findIndex(c => c.id === id);
+
+        const idx = findCardIndexFromHTMLid(card.id);
+
         if (idx !== -1 ){
             cardsOnTheBoard.splice(idx, 1);
         }
@@ -205,16 +222,65 @@ function RemoveSelectedCards(){
 }// RemoveSelectedCards
 
 /**
+ * Checks if two cards are the same by comparing all the four properties
+ * @param card1
+ * @param card2
+ */
+function areCardsTheSame(card1, card2) {
+    return card1.fill === card2.fill &&
+        card1.nrOfShapes === card2.nrOfShapes &&
+        card1.color === card2.color &&
+        card1.shape === card1.share;
+}
+
+/**
+ * Creates an HTML id for use on the board, based on info supplied by a card object.
+ * @param card
+ * @return {string}
+ */
+function createHTMLidFromCardProperties(card){
+    return `${card.shape}|${card.color}|${card.fill}|${card.nrOfShapes}`;
+}
+
+/**
+ * Tries to find a card in the array cardsOnTheBoard based on an HTML id (constructed by the function
+ * createHTMLidFromCardProperties()
+ * @param id
+ * @return {Card|undefined} undefined if not found, a Card if a match is found.
+ */
+function findCardFromHTMLid(id) {
+    return cardsOnTheBoard.find(card => createHTMLidFromCardProperties(card) === id);
+}
+
+/**
+ * Tries to find the index of a Card in the array cardsOnTheBoard based on an HTML id (constructed by the function
+ * createHTMLidFromCardProperties()
+ * @param id
+ * @return {int} -1 if not found, otherwise an index in the array cardsOnTheBoard
+ */
+function findCardIndexFromHTMLid(id) {
+    return cardsOnTheBoard.findIndex(card => createHTMLidFromCardProperties(card) === id);
+}
+
+
+
+/**
  * Find a number of new random items and add them to the DOM.
  * The general CLICK-eventhandler on the parent (div.container) can handle the clicks
- * @constructor
+ * @returns Promise ; use .then() to wait for the results
  */
 function addNewRandomCards(nrOfNewCards) {
-    GetRandomItems(nrOfNewCards).then(cards => {
-        console.log(cards);
-        ProcessCardsFromBackend(cards);
-        addCards(cards);
-        showCardsLeftInFullSet();
+    return new Promise((resolve, reject) => {
+        GetRandomItems(nrOfNewCards).then(
+            (cards) => {
+                ProcessCardsFromBackend(cards);
+                addCards(cards);
+                showCardsLeftInFullSet();
+                resolve();
+            },
+            (error) => {
+                console.log(error);
+            });
     });
 }// AddNewRandomCards
 
@@ -226,11 +292,10 @@ function isSelectionASet() {
     const selectedCardsGUI = document.querySelectorAll('div.card.selected');
     const cardsInfo = [];
 
-    // collect info on selected cards
+    // collect info on selected HTML-div's containing a graphical representation of a card.
     for (const card of selectedCardsGUI) {
-        const id = parseInt(card.id, 10);
-        const cardInfo = cardsOnTheBoard.find(c => c.id === id);
-        if (cardInfo) {
+        const cardInfo = findCardFromHTMLid(card.id);
+        if (cardInfo !== undefined) {
             cardsInfo.push(cardInfo);
         }
     }
@@ -407,33 +472,60 @@ function showHintsAsCards(hints) {
     }
 }// showHintsAsCards
 
+/**
+ * Find all possible sets by using backtracking.
+ * @param cards the set of cards to investigate
+ * @param currentSet the current set under investigation
+ * @return {[]|*[]} an array comprising of valid sets
+ */
 function findAllSet(cards, currentSet){
 
+    // for this level, clear the sets found
     const setsFound = [];
 
+    // make a collection of cards not currently in the set 'currentSet' so we know which cards remain
+    // to be investigated
     const otherCards = cards.filter(c => currentSet.find(c2 => c2.id === c.id) === undefined);
 
+    // do we have a set of 3 cards? if not, keep collecting; if yes, check if it is a set.
     if (currentSet.length < 3) {
+
+        // check if there are other cards left to be investigated
         if (otherCards.length >0){
+            // cycle through all the 'other cards' using a for-loop on this level.
             for (let i = 0; i < otherCards.length-1; i++) {
+                // get a new card
                 const newcard = otherCards[i];
+
+                // if we remove this card, is there anything left to check?
+                // because all the cards "on the left of the card taken" do not need to be investigated
+                // again
                 let remainingToCheck;
                 if (otherCards.length > 1) {
+                    // construct a set of cards to be investigated in the next recursive call by
+                    // making a copy of a slice of the cards "on the right of the taken card".
                     remainingToCheck = otherCards.slice(i+1);
                 }
                 else{
                     remainingToCheck = [];
                 }
+                // start recursing the remaining cards. first extend the current set with the new
+                // card taken, and construct a new set for the next level.
                 const setsFoundBelow = findAllSet(remainingToCheck, [...currentSet, newcard]);
+
+                // add the sets found on the lower levels to the sets found.
                 setsFound.push(...setsFoundBelow);
             }
         }
     }
     else {
         if (isCardsetASet(currentSet, false)) {
+            // return values must always be an array, so surround the result in brackets [] to construct
+            // an array with one value.
             return [currentSet];
         }
     }
+
     return setsFound;
 }// findAllSet
 
