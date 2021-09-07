e72842662964f6b9f9ae1c1e016fecc0618907ac
Joris Lops <joris.lops@nhlstenden.com>
2021-09-07 14:27:32 +0200

    renamed files

--- Backend/V1/Backend/TestBackend/GameControllerTests.cs
+++ Backend/V1/Backend/TestBackend/GameControllerTests.cs
@@ -1,22 +1,12 @@
 using System;
-using System.Dynamic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Backend;
 using Backend.Models;
 using FluentAssertions;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
-using Newtonsoft.Json;
-
 using NUnit.Framework;
-
 
 namespace TestBackend
 {
-    
     public class GameControllerTests : IntegrationTest
     {
         [Test]
@@ -87,60 +77,6 @@ namespace TestBackend
             
             nextHand = await GetRequest<Card[]>("/Game/DrawCards", new {gameId = 1, numberOfCards = 3});
             nextHand.Should().HaveCount(0);
-        }
-    }
-
-    public class IntegrationTest
-    {
-        private TestServer _server;
-        private HttpClient _client;
-
-        [SetUp]
-        public void Setup()
-        {
-            _server = new TestServer(new WebHostBuilder()
-                .UseStartup<Startup>());
-            _client = _server.CreateClient();
-        }
-        
-        protected async Task<T> GetRequest<T>(string apiPath, dynamic parameters)
-        {
-            if (apiPath.StartsWith("/"))
-            {
-                apiPath = apiPath.Substring(1);
-            }
-
-            var requestUri = $"http://localhost:5000/{apiPath}";
-            bool firstParameter = true;
-            foreach (var parameter in parameters.GetType().GetProperties())
-            {
-                var propertyName = parameter.Name;
-                var propretyValue = parameters.GetType().GetProperty(propertyName).GetValue(parameters, null);
-
-                if (firstParameter)
-                {
-                    
-                    requestUri += "?";
-                }
-
-                if (!firstParameter)
-                {
-                    requestUri += "&";
-                }
-
-                requestUri += $"{propertyName}={propretyValue}";
-                
-                firstParameter = false;
-            }
-            
-            var response = await _client.GetAsync(requestUri);
-
-            response.EnsureSuccessStatusCode();
-
-            string json = await response.Content.ReadAsStringAsync();
-
-            var objects = JsonConvert.DeserializeObject<T>(json);
-            return objects;
         }
     }
 }
