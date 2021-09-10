a49d796c8628a07f34e070de4f35a8df19cc77fb
Joris Lops <joris.lops@nhlstenden.com>
2021-09-10 19:56:50 +0200

    Added Default Connection String, retrieved by IConfiguration (Dependency Injection)

--- Backend/V2/Backend/Backend/Controllers/CardController.cs
+++ Backend/V2/Backend/Backend/Controllers/CardController.cs
@@ -1,10 +1,14 @@
 using Backend.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Backend.Controllers
 {
     [Route("[controller]")]
     public class CardController : CrudController<Card>
     {
+        public CardController(IConfiguration configuration) : base(configuration)
+        {
+        }
     }
 }
