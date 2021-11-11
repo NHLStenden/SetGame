a3e715b5232c2a3df1362770d4878cbb18567999
Joris Lops <joris.lops@nhlstenden.com>
2021-11-11 20:01:26 +0100

    changed update method

--- Backend/V4/Backend/Backend/Repository/GenericRepository.cs
+++ Backend/V4/Backend/Backend/Repository/GenericRepository.cs
@@ -8,42 +8,45 @@ namespace Backend.Repository
     public class GenericRepository<T> : IGenericRepository<T> where T : class, new()
     {
         protected readonly SetContext Db;
+        protected readonly DbSet<T> DbSet;
 
         public GenericRepository(SetContext db)
         {
             Db = db;
+            DbSet = Db.Set<T>();
         }
         
         public async Task<T> GetByIdAsync(int id)
         {
-            var entity = await Db.Set<T>().FindAsync(id);
+            var entity = await DbSet.FindAsync(id);
             return entity;
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
         {
-            var entities = await Db.Set<T>().AsNoTracking().ToListAsync();
+            var entities = await DbSet.AsNoTracking().ToListAsync();
             return entities;
         }
 
         public async Task<bool> DeleteAsync(int id)
         {
             var entityToDelete = await GetByIdAsync(id);
-            Db.Set<T>().Remove(entityToDelete);
+            DbSet.Remove(entityToDelete);
             int numRowEffected = await Db.SaveChangesAsync();
             return numRowEffected > 0;
         }
 
         public async Task<bool> AddAsync(T entity)
         {
-            await Db.Set<T>().AddAsync(entity);
+            await DbSet.AddAsync(entity);
             var numRowsEffected = await Db.SaveChangesAsync();
             return numRowsEffected > 0;
         }
 
         public async Task<bool> UpdateAsync(T entity)
         {
-            Db.Set<T>().Update(entity);
+            DbSet.Attach(entity);
+            DbSet.Update(entity);
             var numRowsEffected = await Db.SaveChangesAsync();
             return numRowsEffected > 0;
         }
