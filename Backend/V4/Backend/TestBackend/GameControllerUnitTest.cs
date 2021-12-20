58954ce92f38c1d5b8866860489bb8011556236a
Joris Lops <joris.lops@nhlstenden.com>
2021-12-20 15:50:45 +0100

    Add Unit Test for GameController, Test StartNewGame and use mocks (NSubstitute).

--- Backend/V4/Backend/TestBackend/GameControllerUnitTest.cs
+++ Backend/V4/Backend/TestBackend/GameControllerUnitTest.cs
@@ -1,5 +1,16 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Backend.Controllers;
+using Backend.DTOs;
+using Backend.Models;
+using Backend.Repository;
+using Backend.Services;
+using FluentAssertions;
+using NSubstitute;
 using Xunit;
+using YamlDotNet.Serialization.NodeTypeResolvers;
 
 namespace TestBackend
 {
@@ -8,16 +19,45 @@ namespace TestBackend
         [Fact]
         public async Task CheckSet_InvalidNumberOfCards_ArgumentException()
         {
-            // var deckService = new Mock<IDeckService>();
-            // var gameRepository = new Mock<IGameRepository>();
-            // var playerRepository = new Mock<IPlayerRepository>();
-            // var setService = new Mock<ISetService>();
-            // var sut = new GameService(deckService.Object, gameRepository.Object, 
-            //                             playerRepository.Object, setService.Object);
-            //
-            // Func<Task> checkSet = async () => await sut.CheckSet(1, new []{1,2});
-            //
-            // await checkSet.Should().ThrowAsync<ArgumentException>();
+            var game = new Game()
+            {
+                CardIndex = -1, CardsOnTable = new List<CardOnTable>()
+                {
+                    new CardOnTable()
+                    {
+                        Card = new Card()
+                            { Color = Color.Green, Fill = Fill.Solid, Shape = Shape.Pill, Id = 1, NrOfShapes = 3 }
+                    }
+                }
+            };
+
+            var gameDto = new GameDto()
+            {
+                CardsOnTable = game.CardsOnTable.Select(x => x.Card).ToList(),
+                Complexity = 10, PlayerId = 1, PlayerName = "Joris"
+            };
+            
+            
+            var uof = Substitute.For<IUnitOfWork>();
+            var gameRepository = Substitute.For<IGameRepository>();
+            var gameService = Substitute.For<IGameService>();
+            gameService.StartNewGame(1).Returns(game);
+            
+            
+            var mapper = Substitute.For<IMapper>();
+            mapper.Map<GameDto>(game).Returns(gameDto);
+
+            //arrange
+            var sut = new GameController(uof, gameRepository, gameService, mapper);
+
+            //act
+            var resultGameDto = await sut.StartNewGame(1);
+
+            //asserts
+            gameService.Received().StartNewGame(1);
+            uof.Received().SaveChangesAsync();
+            mapper.Received().Map<GameDto>(game);
+            resultGameDto.Should().BeEquivalentTo(gameDto);
         }
         
         
